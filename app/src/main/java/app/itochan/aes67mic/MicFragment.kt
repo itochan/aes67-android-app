6c41a2946f6fa4d2d5149d52cfbf1e8a329bd1fc
Kazunori Jo <itochan315@gmail.com>
2019-05-24 00:43:09 +0900

    Rename to MicFragment

--- app/src/main/java/app/itochan/aes67mic/MicFragment.kt
+++ app/src/main/java/app/itochan/aes67mic/MicFragment.kt
@@ -5,15 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import app.itochan.aes67mic.databinding.FragmentMainBinding
+import app.itochan.aes67mic.databinding.FragmentMicBinding
 
-class MainFragment : Fragment() {
+class MicFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMainBinding.inflate(inflater, container, false)
+        val binding = FragmentMicBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
