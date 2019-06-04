c2e5580bbc62ba77cba85b91f400f0689863f05c
Kazunori Jo <itochan315@gmail.com>
2019-06-05 01:25:14 +0900

    Restore toolbar

--- app/src/main/java/app/itochan/aes67/MainActivity.kt
+++ app/src/main/java/app/itochan/aes67/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         textMessage = findViewById(R.id.message)
