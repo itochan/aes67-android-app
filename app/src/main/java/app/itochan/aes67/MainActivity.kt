b91ecb6ede3372d0a2ce180932980cb75d3f392b
Kazunori Jo <itochan315@gmail.com>
2019-06-05 01:14:26 +0900

    Make empty Bottom Navigation Activity

--- app/src/main/java/app/itochan/aes67/MainActivity.kt
+++ app/src/main/java/app/itochan/aes67/MainActivity.kt
@@ -1,22 +1,38 @@
 package app.itochan.aes67
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    textMessage.setText(R.string.title_home)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    textMessage.setText(R.string.title_dashboard)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    textMessage.setText(R.string.title_notifications)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
-
 }
