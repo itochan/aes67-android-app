c9b6a99530ca35aaaa573f15a706769929eabedd
Kazunori Jo <itochan315@gmail.com>
2019-06-06 01:07:11 +0900

    Use Jetpack Navigation

--- app/src/main/java/app/itochan/aes67/MainActivity.kt
+++ app/src/main/java/app/itochan/aes67/MainActivity.kt
@@ -1,35 +1,25 @@
 package app.itochan.aes67
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener =
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_receiver -> {
-                    textMessage.setText(R.string.title_receiver)
-                    true
-                }
-                R.id.navigation_transmitter -> {
-                    textMessage.setText(R.string.title_transmitter)
-                    true
-                }
-                else -> false
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+    }
 
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
