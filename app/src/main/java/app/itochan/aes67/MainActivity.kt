14595161d489cfbf963da5c173dd2741ca036a17
Kazunori Jo <itochan315@gmail.com>
2019-06-06 16:16:19 +0900

    Refactor more simply

--- app/src/main/java/app/itochan/aes67/MainActivity.kt
+++ app/src/main/java/app/itochan/aes67/MainActivity.kt
@@ -2,8 +2,8 @@ package app.itochan.aes67
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -12,14 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        val navController = findNavController(R.id.nav_host_fragment)
-        setupActionBarWithNavController(navController)
-        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController)
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
