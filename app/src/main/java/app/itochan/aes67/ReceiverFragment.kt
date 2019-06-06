5b1c1385876ce7fa5c54cf09ca56dcbb6cb691a0
Kazunori Jo <itochan315@gmail.com>
2019-06-06 18:23:27 +0900

    Create ReceiverViewModel

--- app/src/main/java/app/itochan/aes67/ReceiverFragment.kt
+++ app/src/main/java/app/itochan/aes67/ReceiverFragment.kt
@@ -1,5 +1,15 @@
 package app.itochan.aes67
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 
-class ReceiverFragment : Fragment()
+class ReceiverFragment : Fragment() {
+
+    private val viewModel by viewModels<ReceiverViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycle.addObserver(viewModel)
+    }
+}
---
+++ app/src/main/java/app/itochan/aes67/ReceiverViewModel.kt
@@ -0,0 +1,8 @@
+package app.itochan.aes67
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+
+class ReceiverViewModel : ViewModel(), LifecycleObserver {
+    // TODO: Implement the ViewModel
+}
