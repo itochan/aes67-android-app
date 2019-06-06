89d33957163bd0330111bb7f2fc438279fd9ba76
Kazunori Jo <itochan315@gmail.com>
2019-06-07 02:49:49 +0900

    Add UI parts

--- app/src/main/java/app/itochan/aes67/ReceiverFragment.kt
+++ app/src/main/java/app/itochan/aes67/ReceiverFragment.kt
@@ -1,8 +1,12 @@
 package app.itochan.aes67
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import app.itochan.aes67.databinding.FragmentReceiverBinding
 
 class ReceiverFragment : Fragment() {
 
@@ -11,5 +15,16 @@ class ReceiverFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycle.addObserver(viewModel)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentReceiverBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+            it.viewModel = viewModel
+        }.root
     }
 }
--- app/src/main/java/app/itochan/aes67/ReceiverViewModel.kt
+++ app/src/main/java/app/itochan/aes67/ReceiverViewModel.kt
@@ -1,8 +1,35 @@
 package app.itochan.aes67
 
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.net.wifi.WifiManager
+import androidx.core.content.getSystemService
+import androidx.lifecycle.*
 
-class ReceiverViewModel : ViewModel(), LifecycleObserver {
-    // TODO: Implement the ViewModel
+
+class ReceiverViewModel(application: Application) : AndroidViewModel(application),
+    LifecycleObserver {
+
+    private lateinit var multicastLock: WifiManager.MulticastLock
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        acquireMulticastPacket()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        multicastLock.release()
+    }
+
+    private fun acquireMulticastPacket() {
+        val wifiManager = getApplication<Application>().getSystemService<WifiManager>()!!
+        multicastLock = wifiManager.createMulticastLock(MULTICAST_TAG).apply {
+            setReferenceCounted(true)
+            acquire()
+        }
+    }
+
+    companion object {
+        private val MULTICAST_TAG: String = ReceiverViewModel::class.java.name
+    }
 }
