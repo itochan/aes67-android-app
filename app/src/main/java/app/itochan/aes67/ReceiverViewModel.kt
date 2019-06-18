86d305447cb3a0c16d74b73c5ba9a2d35c4148b2
Kazunori Jo <itochan315@gmail.com>
2019-06-18 17:26:56 +0900

    Receive Multicast packet

--- app/src/main/java/app/itochan/aes67/ReceiverViewModel.kt
+++ app/src/main/java/app/itochan/aes67/ReceiverViewModel.kt
@@ -2,23 +2,35 @@ package app.itochan.aes67
 
 import android.app.Application
 import android.net.wifi.WifiManager
+import android.util.Log
 import androidx.core.content.getSystemService
 import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.net.DatagramPacket
+import java.net.InetAddress
+import java.net.MulticastSocket
 
 
 class ReceiverViewModel(application: Application) : AndroidViewModel(application),
     LifecycleObserver {
 
     private lateinit var multicastLock: WifiManager.MulticastLock
+    private lateinit var socket: MulticastSocket
+    private lateinit var group: InetAddress
+    private var receiverJob: Job? = null
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
         acquireMulticastPacket()
+        startReceiver()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
         multicastLock.release()
+        stopReceiver()
     }
 
     private fun acquireMulticastPacket() {
@@ -29,7 +41,33 @@ class ReceiverViewModel(application: Application) : AndroidViewModel(application
         }
     }
 
+    private fun startReceiver() {
+        socket = MulticastSocket(AES67_PORT).apply {
+            group = InetAddress.getByName(MULTICAST_ADDRESS)
+            joinGroup(group)
+        }
+
+        receiverJob = viewModelScope.launch(Dispatchers.Default) {
+            while (true) {
+                val buf = ByteArray(PACKET_LENGTH)
+                val recv = DatagramPacket(buf, buf.size, group, AES67_PORT)
+                socket.receive(recv)
+            }
+        }
+    }
+
+    private fun stopReceiver() {
+        socket.apply {
+            leaveGroup(group)
+            close()
+        }
+        receiverJob?.cancel()
+    }
+
     companion object {
         private val MULTICAST_TAG: String = ReceiverViewModel::class.java.name
+        private const val MULTICAST_ADDRESS = "239.69.128.165"
+        private const val AES67_PORT = 5004
+        private const val PACKET_LENGTH = 342
     }
 }
